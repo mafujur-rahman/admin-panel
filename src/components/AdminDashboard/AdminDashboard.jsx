8279401676ca8619a49dacae5dd5870acb7db2d4
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-02-05 19:42:45 +0600

    designing basic admin dashboard

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -2,8 +2,20 @@
 
 const AdminDashboard = () => {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="bg-[#FEA4A5] flex justify-center items-center">
+            <div className="container min-h-screen flex ">
+
+                {/* Sidebar */}
+                <div className="w-1/5 bg-[#EE4E5B] p-5 ">
+                    <h1 className="text-white font-bold text-lg">Sidebar</h1>
+                </div>
+
+                {/* Main Content */}
+                <div className="w-4/5 bg-white p-5">
+                    <h1 className="text-gray-800 font-bold text-lg">Main Content</h1>
+                </div>
+
+            </div>
         </div>
     );
 };
